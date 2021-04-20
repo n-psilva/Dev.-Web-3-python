cda9ecfd2b17f9a1929fcb4afeb803ae0f0b5461
npsilva <>
2021-04-20 02:05:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+# -*- coding:utf-8 -*-
+
+# 1
+'''def comprimento(saudacao, nome):
+  print(f'{saudacao} {nome}!')
+
+saud = input('Entre com uma saudação: ')
+nome = input('Entre com o primeiro nome: ')
+comprimento(saud,nome)'''
+
+# 2
+'''def soma(num1, num2, num3):
+  adc = num1+num2+num3
+  print('A soma dos 3 números é de {}'.format(adc))
+
+n1 = int(input('Entre com o primeiro numero:'))
+n2 = int(input('Entre com o segundo numero:'))
+n3 = int(input('Entre com o terceiro numero:'))
+soma(n1,n2,n3)'''
+
+# 3
+'''def aumento(valor, percent):
+  return percent/100*valor+valor
+print(aumento(1000,30))'''
+
+# 4
+'''def fizzbuzz(num):
+  if num%2==0:
+    print('Fizz')
+  elif num%3==0 and num%5==0:
+    print('FizzBuzz')
+  else:
+    print(num)
+
+fizzbuzz(23)'''
+
+# 5
+#def notas():
